060b5cefc9519954a7b25408be92db1777cf5077
Dina Saunders <dina_saunders@mac.com>
2020-12-07 14:15:12 -0500

    updated server file to only require express

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
 
@@ -10,15 +9,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on https://localhost:${PORT}!`);
+  console.log(`App running on https://localhost:${PORT}`);
 });
 
