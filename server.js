030c78d13403dce6025957e3ddbd93aed6ca4c82
Dina Saunders <dina_saunders@mac.com>
2020-12-08 10:51:35 -0500

    updated server with mongoose and api routes

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
 const PORT = process.env.PORT || 3000
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// routes
-// app.use(require("./routes/api-routes.js"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
 
 app.listen(PORT, () => {
   console.log(`App running on https://localhost:${PORT}`);
