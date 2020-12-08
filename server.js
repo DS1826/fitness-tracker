4d1d1e0343e7b9bc7038cacddaaa8697cdcc81a3
Dina Saunders <dina_saunders@mac.com>
2020-12-08 13:14:35 -0500

    corrected duplicate express app variable

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000
 
-const app = express();
-
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
