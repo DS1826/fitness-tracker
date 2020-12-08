0477567a71bf6726a60ef4dfe35177721b357eaa
Dina Saunders <dina_saunders@mac.com>
2020-12-08 18:51:33 -0500

    added put route for exercise data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,13 +5,23 @@ module.exports = function(app) {
     app.get("/api/workouts", function (req, res) {
         Workout.find()
         .then(function(data) {
+            console.log(data);
             res.json(data);
         });
     });
 
     // Add a new workout
     app.post("/api/workouts", function (req, res) {
+        console.log(req.body);
+
         Workout.create({})
+        .then(function(data) {
+            res.json(data);
+        });
+    });
+
+    app.put("/api/workouts/:id", function (req, res) {
+        Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
         .then(function(data) {
             res.json(data);
         });
