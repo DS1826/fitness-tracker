598021b697e7b089291b80d4668c62c1092f5540
Dina Saunders <dina_saunders@mac.com>
2020-12-08 18:50:51 -0500

    revised model syntax

--- models/workout.js
+++ models/workout.js
@@ -4,13 +4,29 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
     day: { type: Date, default: Date.now },
     exercises: [
-        { type: String },
-        { name: String },
-        { duration: Number },
-        { distance: Number },
-        { weight: Number },
-        { reps: Number },
-        { sets: Number },
+        {
+            type: { 
+                type: String
+            },
+            name: {
+                type: String
+            }, 
+            duration: {
+                type: Number
+            }, 
+            distance: {
+                type: Number
+            }, 
+            weight: {
+                type: Number
+            }, 
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
     ]
 });
 
