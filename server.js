18c73b67a005304d6ae9a0b31dc0785188819181
Dina Saunders <dina_saunders@mac.com>
2020-12-08 18:50:18 -0500

    reorganized server code

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true
 });
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on https://localhost:${PORT}`);
