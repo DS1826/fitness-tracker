829cf35848e3c383daf603f9ed3ff972445153da
Dina Saunders <dina_saunders@mac.com>
2020-12-08 13:42:04 -0500

    adding api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const Workout = require("../models/workout");
+
+module.exports = function(app) {
+    // Get all workouts
+    app.get("/api/workouts", function (req, res) {
+        Workout.find()
+        .then(function(data) {
+            res.json(data);
+        });
+    });
+
+    // Add a new workout
+    app.post("/api/workouts", function (req, res) {
+        Workout.create({})
+        .then(function(data) {
+            res.json(data);
+        });
+    });
+
+    // Get workout in a specific range
+    app.get("/api/workouts/range", function (req, res) {
+        Workout.find()
+        .then(function(data) {
+            res.json(data)
+        });
+    });
+}
